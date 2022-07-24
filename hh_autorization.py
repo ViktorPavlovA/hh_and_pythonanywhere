4d6e199789730aa06fd47409f5219a26f65b206a
Viktor Pavlov <viktor.pavlov.1998@list.ru>
2022-07-24 14:59:36 +0300

    Update hh_autorization.py

--- hh_autorization.py
+++ hh_autorization.py
@@ -1,13 +1,12 @@
 #don't fogget write mail and password in:  if __name__ == '__main__':
-#up_our_resume2(browser) < -- if you haven't second resume comment it
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 from random import randint
 
-class hh_client:
-    def login_hh(self,password,login):
+class hh_client():
+    def login_hh(self,password,login,n):
         def click_1(browser):
             """
                                                      CSS_code for example
@@ -31,40 +30,37 @@ class hh_client:
             __login_button = browser.find_element(By.CSS_SELECTOR,"button[class ='bloko-button bloko-button_kind-primary'][type='submit'][data-qa='account-login-submit']")
             __login_button.click()
             sleep(randint(1, 2))
-        def up_our_resume1(browser):
-            __resume_click =browser.find_element(By.CSS_SELECTOR,"a[data-page-analytics-experiment-event ='resume_list_header']")
-            __resume_click.click()
+        def up_our_resume(browser):
+            try:
+                __resume_click = browser.find_element(By.CSS_SELECTOR,
+                                                      "a[data-page-analytics-experiment-event ='resume_list_header']")
+                __resume_click.click()
+            except:
+                __out_click = browser.find_element(By.CSS_SELECTOR,
+                                                   "button[class ='bloko-button bloko-button_stretched bloko-button_appearance-outlined'")
             sleep(randint(1, 2))
-            __button_up = browser.find_element(By.CSS_SELECTOR,"button[data-qa='resume-update-button'][class='bloko-link']")
+            __button_up = browser.find_element(By.XPATH, "//button[. = 'Поднять в поиске']")
             __button_up.click()
             sleep(randint(1, 2))
-
-        def up_our_resume2(browser):
-            __resume_click = browser.find_element(By.CSS_SELECTOR,"a[data-page-analytics-experiment-event ='resume_list_header']")
-            __resume_click.click()
-            sleep(randint(1, 2))
-            __button_up = browser.find_element(By.CSS_SELECTOR,"button[class='bloko-button bloko-button_kind-secondary bloko-button_scale-small bloko-button_stretched'][data-qa='resume-update-button']")
-            __button_up.click()
-            sleep(randint(1, 2))
-
-
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         browser = webdriver.Chrome(options=chrome_options)
         browser.get("https://spb.hh.ru/account/login?backurl=%2F&hhtmFrom=main")
-        click_1(browser)
-        click_and_auth(browser,password,login)
-        up_our_resume1(browser)
-        up_our_resume2(browser) #if you have 1 resume comment it with using #
-        browser.close()
+        for _ in range(n):
+            click_1(browser)
+            click_and_auth(browser,password,login)
+            up_our_resume(browser)
+            browser.close()
+
 
 
 if __name__ == '__main__':
     #write here your email and password
-    loginhh = "email"
-    passwordhh = "password"
+    loginhh = "your_login"
+    passwordhh = "your_password"
+    n = 2 #count of resume
     hh = hh_client()
     while True:
         for i in range(14400):
@@ -73,11 +69,11 @@ if __name__ == '__main__':
             sleep(1)
             if i == 14400 - 1:
                 try:
-                    hh.login_hh(passwordhh, loginhh)
+                    hh.login_hh(passwordhh, loginhh,n)
                     print(f"Up resume")
                 except:
                     try:
-                        hh.login_hh(passwordhh, loginhh)
+                        hh.login_hh(passwordhh, loginhh,n)
                     except: pass
 
 
